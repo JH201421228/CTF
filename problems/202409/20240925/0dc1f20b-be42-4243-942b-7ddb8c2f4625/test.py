c6d04b558927f72641cd0fa06cc8a700dc2aa99e
박주헌 <741u741@naver.com>
2024-09-25 21:31:06 +0900

    20240925 #1

---
+++ problems/202409/20240925/0dc1f20b-be42-4243-942b-7ddb8c2f4625/test.py
@@ -0,0 +1,14 @@
+users = {
+    '0': {  
+        'userid': 'guest',
+        'level': 1,
+        'password': 'guest'
+    },
+    '1': {
+        'userid': 'admin',
+        'level': 9999,
+        'password': 'admin'
+    }
+}
+
+print(users["guest"])
